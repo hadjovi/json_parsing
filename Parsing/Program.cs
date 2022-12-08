9de158ebb7d00625124008f5a2e0fe954493bb04
Hursen Adjovi <hadjovi@ksu.edu>
2022-12-07 19:08:59 -0600

    jsonCode

---
+++ Parsing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Parsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+
+            using (StreamReader reader = new StreamReader("wikidata_sample.json"))
+            {
+                string jsonFile = reader.ReadToEnd();
+
+                JsonDocument jDoc = JsonSerializer.Deserialize<JsonDocument>(jsonFile);
+                var labels = jDoc.RootElement.GetProperty("labels");
+                var en = labels.GetProperty("en");
+                var value = en.GetProperty("value");
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
