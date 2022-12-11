86f20cf5e13436a97c62888be44fbe3b9828d99c
Hursen <hadjovi@ksu.edu>
2022-12-10 18:05:59 -0600

    NewVersion

--- Parsing/Program.cs
+++ Parsing/Program.cs
@@ -7,18 +7,54 @@ namespace Parsing
     class Program
     {
         static void Main(string[] args)
-        {            
-
+        {
+            /*
             using (StreamReader reader = new StreamReader("wikidata_sample.json"))
             {
                 string jsonFile = reader.ReadToEnd();
+
+                JsonDocument jDoc = JsonDocument.Parse(jsonFile);
+
+                var labels = jDoc.RootElement.GetProperty("labels");
 
                 //JsonDocument jDoc = JsonSerializer.Deserialize<JsonDocument>(jsonFile);
                 //var labels = jDoc.RootElement.GetProperty("labels");
                 //var en = labels.GetProperty("en");
                 // value = en.GetProperty("value");
-                Console.WriteLine(jsonFile);
+                Console.WriteLine(labels);
+            }*/
+
+            //Check if the code works with one word
+            using (StreamReader reader = new StreamReader("oneWord_sample.json"))
+            {
+                string jsonFile = reader.ReadToEnd();
+
+                JsonDocument jDoc = JsonDocument.Parse(jsonFile);
+
+                var labels = jDoc.RootElement.GetProperty("labels");
+
+                //string[] langs = { "en", "el", "fr" };
+
+                var en = labels.GetProperty("en");
+
+                var value = en.GetProperty("value");
+
+                /*
+                foreach(string l in langs)
+                {
+                    var lang = labels.GetProperty(l);
+                    var value = lang.GetProperty("value");
+                    Console.WriteLine(value);
+                }
+                */
+
+                //JsonDocument jDoc = JsonSerializer.Deserialize<JsonDocument>(jsonFile);
+                //var labels = jDoc.RootElement.GetProperty("labels");
+                //var en = labels.GetProperty("en");
+                // value = en.GetProperty("value");
+                Console.WriteLine(value);
             }
         }
     }
 }
+
